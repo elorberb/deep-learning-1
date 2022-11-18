fca8193c04576d72144963adaac49eeab4871e00
elorberb <etay.lorberboym@intel.com>
2022-11-18 14:31:20 +0200

    finish functions without testing

--- ass1_code.py
+++ ass1_code.py
@@ -3,8 +3,10 @@ import os
 import matplotlib.pyplot as plt
 import time
 from sklearn.model_selection import train_test_split
+import operator
 
-np.random.seed(314977596)
+seed = 314977596
+np.random.seed(seed)
 
 
 def initialize_parameters(layer_dims: list) -> dict:
@@ -296,9 +298,163 @@ def update_parameters(parameters, grads, learning_rate):
     return parameters
 
 
-def l_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size):
-    pass
+def l_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size, batch_norm):
+    """
+    Description:
+    Implements a L-layer neural network.All layers but the last should have
+    the ReLU activation function, and the final layer will apply the softmax activation function.
+    The size of the output layer should be equal to the number of labels in the data.
+    Please select a batch size that enables your
+    code to run well (i.e. no memory overflows while still running relatively fast).
+
+    @param X: the input data, a numpy array of shape (height*width , number_of_examples)
+    Comment: since the input is in grayscale we only have height and width, otherwise it would have been height*width*3
+    @param Y: the “real” labels of the data, a vector of shape (num_of_classes, number of examples)
+    @param layers_dims: a list containing the dimensions of each layer, including the input
+    @param learning_rate: the learning rate used to update the parameters (the “alpha”)
+    @param num_iterations: number of epoches we iterate over the data.
+    @param batch_size: the number of examples in a single training batch.
+    @param batch_norm: bool to indicate if user wants to normalize the data.
+
+    @return:
+    parameters – the parameters learnt by the system during
+    the training (the same parameters that were updated in the update_parameters function).
+    costs – the values of the cost function (calculated by the compute_cost function).
+    One value is to be saved after each 100 training iterations (e.g. 3000 iterations -> 30 values).
+
+    """
+    X_train, X_test, y_train, y_test = split_preprocess_train_data(X, Y)
+    parameters = initialize_parameters(layers_dims)
+    train_iter, epoch = 0, 0
+    stop_early = False
+    costs, graph_costs = [], [[], []]
+    indices = np.arange(len(Y[0, :]))
+
+    while train_iter < num_iterations and not stop_early:
+        print(f'Current epoch = {epoch}')
+        batch_begin = 0
+        batch_finish = batch_size
+        shuffled_ind = np.copy(indices)
+        np.random.shuffle(shuffled_ind)
+        accuracy_before, accuracy = 0, 0
+
+        while batch_begin < num_iterations and not stop_early:
+            # split data to batch
+            X_batch, y_batch = create_batch_data(batch_begin, batch_finish, shuffled_ind, X, Y)
+            # perform forward propagation
+            AL, caches = l_model_forward(X_batch, parameters, batch_norm)
+            #  values needs to be saved each 100 training iterations (e.g. 3000 iterations -> 30 values).
+            if train_iter % 100 == 0:
+                cost = compute_cost(AL, y_batch)
+                costs.append(cost)
+                graph_costs[0].append(train_iter)
+                graph_costs[1].append(cost)
+
+                accuracy = predict(X_test, y_test, parameters, batch_norm)
+                print(f"Current iteration = {train_iter} , Cost = {cost}, Accuracy = {accuracy}")
+                if check_stopping_criterion(accuracy_before, accuracy):
+                    stop_early = True
+                    continue
+
+                accuracy_before = accuracy
+            grads = l_model_backward(AL, y_batch, caches)
+            parameters = update_parameters(parameters, grads, learning_rate)
+            # update amount of data trained
+            batch_begin, batch_finish = batch_begin + batch_size, batch_finish + batch_size
+            train_iter += 1
+        epoch += 1
+    return parameters, costs, graph_costs, train_iter
+
+
+def check_stopping_criterion(accuracy_before, current_accuracy):
+    """
+
+    @param accuracy_before: Accuracy level last iteration
+    @param current_accuracy: Accuracy level current iteration
+    @return:
+    bool representing whether to stop training
+    """
+    return accuracy_before > current_accuracy + 0.01
+
+
+def create_batch_data(batch_begin, batch_finish, shuffled_ind, X, Y):
+    """
+
+    @param batch_begin: start ind for batch
+    @param batch_finish: end idx for bach
+    @param shuffled_ind:  shuufles indices
+    @param X: X data
+    @param Y: Y data
+    @return:
+    data split into batch
+    """
+    if batch_finish > len(shuffled_ind):
+        batch_indices = shuffled_ind[batch_begin:len(shuffled_ind)]
+    else:
+        batch_indices = shuffled_ind[batch_begin:batch_finish]
+    X_batch = X[:, batch_indices]
+    y_batch = Y[:, batch_indices]
+
+    return X_batch, y_batch
+
+
+def split_preprocess_train_data(X, Y):
+    """
+
+    @param X: the X_train data from keras
+    @param Y: the y_train data from keras
+    @return:
+    data preprocessed and split into training and validation
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=seed)
+
+    X_train = X_train.reshape(X_train.shape[0], -1).T  # The "-1" makes reshape flatten the remaining dimensions
+    X_test = X_test.reshape(X_test.shape[0], -1).T
+
+    X_train = np.divide(X_train, 255)
+    X_test = np.divide(X_test, 255)
+
+    y_train = np.squeeze(np.eye(y_train.max() + 1)[y_train.reshape(-1)]).T
+    y_test = np.squeeze(np.eye(y_test.max() + 1)[y_test.reshape(-1)]).T
+
+    return X_train, X_test, y_train, y_test
+
+
+def split_preprocess_test_data(X_test, y_test):
+    """
+
+    @param X_test: X testing data from keras
+    @param y_test: y testing data from keras
+    @return:
+    data preprocessed for testing model
+    """
+    X_test = X_test.reshape(X_test.shape[0], -1).T
+    X_test = np.divide(X_test, 255)
+    y_test = np.squeeze(np.eye(y_test.max() + 1)[y_test.reshape(-1)]).T
+
+    return X_test, y_test
 
 
 def predict(X, Y, parameters, use_batchnorm):
-    pass
+    """
+
+    @param X: the input data, a numpy array of shape (height*width, number_of_examples)
+    @param Y: the “real” labels of the data, a vector of shape (num_of_classes, number of examples)
+    @param parameters: a python dictionary containing the DNN architecture’s parameters
+    @param use_batchnorm:
+    @return:
+    Accuracy of the NN for the data
+    """
+    count_match = 0
+    prob, caches = l_model_forward(X, parameters, use_batchnorm)
+    prob = softmax(prob)[0]
+    predictions_length = prob.shape[1]
+    for i in range(predictions_length):
+        curr_Y = max(enumerate(Y[:, i]), key=operator.itemgetter(1))[0]
+        curr_prob = max(enumerate(prob[:, i]), key=operator.itemgetter(1))[0]
+
+        if curr_prob == curr_Y:
+            count_match += 1
+    return count_match / Y.shape[1]
+
+
